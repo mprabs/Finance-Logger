17b9cd04809f1b431ca246c6a89b5454f816b516
mprabs <acharyaprabin03@gmail.com>
2021-02-19 15:29:59 +0545

    [FINAL]finace logger

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/payment.js";
+import { Invoice } from "./classes/invoice.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  let values: [string, string, number];
+  values = [toFrom.value, details.value, amount.valueAsNumber];
+
+  let doc: HasFormatter;
+  if (type.value === "invoice") {
+    doc = new Invoice(...values);
+  } else {
+    doc = new Payment(...values);
+  }
+
+  list.render(doc, type.value, "end");
+});
